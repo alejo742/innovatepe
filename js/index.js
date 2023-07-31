00d3ebd7c91e2dc9535069d941077cf654c08b4b
alejo742 <a.manrique742@gmail.com>
2023-07-30 20:32:01 -0500

    landing 1.0

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ const navBurger = document.querySelector('.burger.burger1');
 navBurger.addEventListener('click', ()=> {
     dropdownContent.classList.toggle('visible')
 })
+
+const sendButton = document.getElementById('contact-send-button');
+
+sendButton.addEventListener('click', ()=> {
+    const nameInput = document.getElementById('name-input');
+    const subjectInput = document.getElementById('subject-input');
+    const messageArea = document.getElementById('message-input');
+
+
+    const email = "mailto:elcorreoquequieres@correo.com?Subject="
+
+    var subject = `${encodeURIComponent(subjectInput.value)}&body=`
+    var message = encodeURIComponent("Hola, soy " + nameInput.value + " y quisiera comentarles lo siguiente: \n\n" + messageArea.value)
+
+    var finalString = email + subject + message
+    sendButton.href = finalString
+})
